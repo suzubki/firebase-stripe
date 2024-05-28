eaa3b9c852f7d7c5e0f6bda43f4fb836aa34faa9
Darwin_N19 <darwinccn25@gmail.com>
2024-05-28 10:14:00 -0500

    added initial firebase config

---
+++ src/api/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWpaKXUwYZ86NTzvgxHPDmqED66tDy9NA",
+  authDomain: "fir-stripe-b402a.firebaseapp.com",
+  projectId: "fir-stripe-b402a",
+  storageBucket: "fir-stripe-b402a.appspot.com",
+  messagingSenderId: "948584851710",
+  appId: "1:948584851710:web:d59fb4aa9e84784b5d5daf"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
